6afda53e9faa36a2c3e43b15bc945509e7f2ec39
Aleman023 <103618559+Aleman023@users.noreply.github.com>
2022-04-20 23:36:44 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,6 @@
+
+const App = () => {
+    return "Hola Mundo"
+}
+
+export default  App
---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return (
+    <>
+     <h1>BlockMarket</h1>
+    <nav>
+        <li>
+            <a href="#">Inicio</a>
+        </li>
+        <li>
+            <a href="#">Productos</a>
+        </li>
+        <li>
+            <a href="#">Contacto</a>
+        </li>
+    </nav>
+    </>
+    )
+}
+export default Nav
